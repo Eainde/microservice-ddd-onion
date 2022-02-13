17f2ac4df6ea8fba6e12d602d604a36f99f71d8b
eainde <akshay.dipta@gmail.com>
2022-02-13 17:57:08 +0000

    Adding SwaggerAdding cross origin

--- application/src/main/java/com/eainde/ddd/application/Application.java
+++ application/src/main/java/com/eainde/ddd/application/Application.java
@@ -1,6 +1,7 @@
 package com.eainde.ddd.application;
 
 import com.sun.istack.NotNull;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -20,17 +21,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableJpaRepositories(basePackages = "com.eainde.ddd")
 @EntityScan("com.eainde.ddd.entity")
 public class Application {
-  private static final String LOCALHOST="http://localhost:[*]";
+  private static final String LOCALHOST = "http://localhost:[*]";
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class);
   }
 
   @Bean
-  public WebMvcConfigurer corsConfigureE0(){
+  public WebMvcConfigurer corsConfigureE0() {
     return configureFor(LOCALHOST);
   }
 
-  private WebMvcConfigurer configureFor(String...origins){
+  private WebMvcConfigurer configureFor(String... origins) {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(@NotNull CorsRegistry registry) {
