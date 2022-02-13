6d16aaddf7a146ef289f641920dd04454f52f502
eainde <akshay.dipta@gmail.com>
2022-02-13 17:55:26 +0000

    Adding SwaggerAdding cross origin

--- application/src/main/java/com/eainde/ddd/application/Application.java
+++ application/src/main/java/com/eainde/ddd/application/Application.java
@@ -1,10 +1,14 @@
 package com.eainde.ddd.application;
 
+import com.sun.istack.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "com.eainde.ddd")
 @PropertySource({
@@ -16,7 +20,22 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.eainde.ddd")
 @EntityScan("com.eainde.ddd.entity")
 public class Application {
+  private static final String LOCALHOST="http://localhost:[*]";
   public static void main(String[] args) {
     SpringApplication.run(Application.class);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigureE0(){
+    return configureFor(LOCALHOST);
+  }
+
+  private WebMvcConfigurer configureFor(String...origins){
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry.addMapping("/**").maxAge(3600).allowedMethods("*").allowedOriginPatterns(origins);
+      }
+    };
   }
 }
